ae65f413ec2e1f1cbc2c40d33eab8e615aa5b00c
UDILUCH <chamil.diluksha@pearson.com>
2020-03-16 11:04:04 +0530

    add database connection

--- backend_online_shopping_store/server.js
+++ backend_online_shopping_store/server.js
@@ -1,6 +1,9 @@
 const express = require('express');  
 const cors = require('cors');
 const mongoose = require('mongoose'); // help to connect mongodb database
+const texts = require('./constants/texts');//take constant to prompt messages
+const serverMessages = texts.server;
+
 
 //create express server
 const app = express();
@@ -10,7 +13,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());  // cors middaleware
 app.use(express.json()); // allows to get JSON
 
+//connect to the mongoDB Atlas
+mongoose.connect(serverMessages.MONGODB_URL,
+{
+    useNewUrlParser:true,useUnifiedTopology: true
+})
+.then(() =>{
+    console.log(serverMessages.DB_CONNECTED);
+})
+.catch(()=>{
+    console.log(serverMessages.DB_NOT_CONNECTED);
+});
+
+
 // server listening port
 app.listen(port, () => {
-    console.log('Server is running on port :' + port);
+    console.log(serverMessages.SERVER + port);
 });
