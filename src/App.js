091ef0820f930417ee40c39376e0fb9f3dfbc0e8
ChamilDiluksha <chamildilu@gmail.com>
2020-03-20 12:10:24 +0530

    resolve

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {Component}from 'react';
 import './App.css';
 
-class  extends Component {
+class App extends Component {
   
   render() { 
     return (  
