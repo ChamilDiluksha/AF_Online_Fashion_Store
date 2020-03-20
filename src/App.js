68838f8ec39a00ab2328a97b7df2d11ceec0495a
Prabath Shalitha <prabathshalitha21@gmail.com>
2020-03-20 12:46:45 +0530

    Bootstrap Added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, {Component}from 'react';
 import './App.css';
+// Import bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
-  
-  render() { 
-    return (  
-      <p>hello</p>
+
+  render() {
+    return (
+      <div className="container">
+      <div className="row">
+        <div className="col-md-6">Column 6</div>
+        <div className="col-md-6">Column 6</div>
+        <a href="#" className="btn">Shop Now <i class="fas fa-chevron-right"></i></a>
+      </div>
+    </div>
     );
   }
 }
- 
+
 export default App;
