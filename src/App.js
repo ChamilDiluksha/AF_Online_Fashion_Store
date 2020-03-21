03ffa742bb92682694cba01a8c8ea2fdde47b7d3
Kalpani Abeysinghe <kalpanisandupabaabeysinghe@gmail.com>
2020-03-21 13:54:41 +0530

    Revert "Merge branch 'master' of https://github.com/ChamilDiluksha/AF_Online_Fashion_Store"
    This reverts commit 3b5335d668fc7fea7dbbb14b58b4f4b9ea023501, reversing
    changes made to cba3e35d90baf197fbd0932caf8e2a3bc856f4af.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import React, {Component}from 'react';
-import './App.css';
-// Import bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "./App.css";
 
-
-class App extends Component {
-
-  render() {
-    return (
-      <div className="container">
-      <div className="row">
-        <div className="col-md-6">Column 6</div>
-        <div className="col-md-6">Column 6</div>
-        <a href="#" className="btn">Shop Now <i class="fas fa-chevron-right"></i></a>
-      </div>
-
-    
-    </div>
-    );
-  }
+function App() {
+  return <div className="App">Hello from react</div>;
 }
 
 export default App;
