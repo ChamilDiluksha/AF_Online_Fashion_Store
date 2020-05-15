b1dcb5f751e46adce384ade4991544fb52c436e6
ChamilDiluksha <chamildilu@gmail.com>
2020-05-15 13:33:09 +0530

    remove unnecessary code in manager component

--- src/components/AdminDashboard/NavComponenets/CreateStoreManager.jsx
+++ src/components/AdminDashboard/NavComponenets/CreateStoreManager.jsx
@@ -265,11 +265,9 @@ class StoreManager extends Component {
                 </Form.Group>
             </Form.Row>
 
-            <Form.Group id="formGridCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
+           
 
-            <Button variant="primary" type="submit">
+            <Button variant="primary" type="submit" >
                 Submit
             </Button>
         </Form>
