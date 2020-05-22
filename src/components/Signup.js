4d6a3455b82ff590499efea697c86778f8560b9d
ChamilDiluksha <47770042+ChamilDiluksha@users.noreply.github.com>
2020-05-22 21:23:31 +0530

    handle sigin up issue

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -77,7 +77,7 @@ class Signup extends Component {
 
             <div className="container mt-5">
             <form onSubmit={this.onSubmit}>
-            <Grid textAlign='center' style={{ height: '60vh' }} verticalAlign='middle'>
+            <Grid textAlign='center' style={{ height: '70vh' }} verticalAlign='middle'>
             <Grid.Column style={{ maxWidth: 400 }}>
               <Header as='h2' color='teal' textAlign='center'>
                 Sign-Up 
